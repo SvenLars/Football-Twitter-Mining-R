545ac451140fe41c5a6db91048b5a3ef62a218e0
SvenLars <Sven.Hilbert@posteo.de>
2018-08-27 15:19:43 +0200

    Skript

--- Mining_Script_Main.R
+++ Mining_Script_Main.R
@@ -2,3 +2,17 @@
 #### Twitter Football Text Mining ####
 ######################################
 
+### Load Packages
+
+require(rtweet) 
+require(wordcloud)
+require(stringr)
+
+### Read Tweets and extra functions
+
+getTweets()
+extractText() 
+cleanTweets()
+extractHashtegs()
+extractUsers()
+
